bf3d1ebb958984e3781616d3ce3be3f0b67a0ef1
GunWoo <kkw31025@gmail.com>
2024-09-20 17:36:09 +0900

    [design] - fragment xml 화면 추가

---
+++ app/src/main/java/com/example/kotlinpractice/fragment/MainFragment.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinpractice.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlinpractice.R
+
+class MainFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/kotlinpractice/fragment/QuestionViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinpractice.fragment
+
+class QuestionViewModel {
+}
