2f576299053aa2f1d30d26371eabf42431814814
GunWoo <kkw31025@gmail.com>
2024-09-20 15:58:37 +0900

    [build] - jectpack navigation 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,12 @@ android {
 }
 
 dependencies {
-
+    val nav_version = "2.8.0"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
